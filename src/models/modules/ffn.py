2e582d48c9fb4a43904359b6ca92ad25fef0caba
randy <randytunru@gmail.com>
2026-07-23 21:20:04 +0700

    feat: added GELU ffn

--- src/models/modules/ffn.py
+++ src/models/modules/ffn.py
@@ -1,8 +1,11 @@
+import torch
 from torch import nn
 
-class PositionwiseFeedForward(nn.Module):
+from src.models.modules.components import QuickGELU
+
+class SwiGluFeedForward(nn.Module):
     def __init__(self, d_model, d_ff):
-        super(PositionwiseFeedForward, self).__init__()
+        super(SwiGluFeedForward, self).__init__()
         self.gate_linear = nn.Sequential(
             nn.Linear(d_model, d_ff, bias=False),
             nn.SiLU()
@@ -16,3 +19,14 @@ class PositionwiseFeedForward(nn.Module):
         ff_output = gate_output * up_output
         output = self.down_linear(ff_output)
         return output
+
+class PositionwiseFeedForward(nn.Module):
+    """Position-wise Feed-Forward Network (FFN) used in pre-LLaMa transformer architectures."""
+    def __init__(self, d_model, d_ff):
+        super(PositionwiseFeedForward, self).__init__()
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.activation = QuickGELU() # You can also change to nn.GELU()
+        self.linear2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        return self.linear2(self.activation(self.linear1(x)))
